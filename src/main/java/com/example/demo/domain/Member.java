1d7261fe6fb1009771f62656f4442b3a578b4f69
cutehanchankyo <jeongchangyo71@gmail.com>
2023-01-07 20:12:27 +0900

    add Entity

--- src/main/java/com/example/demo/domain/Member.java
+++ src/main/java/com/example/demo/domain/Member.java
@@ -1,4 +1,38 @@
 package com.example.demo.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String email;
+
+    private String name;
+
+    private String password;
+
+    private String refrashToken;
+
+    @Enumerated(EnumType.STRING) @Column(name = "Role")
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "Role", joinColumns = @JoinColumn(name = "id"))
+    @Builder.Default
+    private List<Role> roles = new ArrayList<>();
+
+    public void updateRefrachToken(String refrashToken){
+        this.refrashToken=refrashToken;
+    }
 }
