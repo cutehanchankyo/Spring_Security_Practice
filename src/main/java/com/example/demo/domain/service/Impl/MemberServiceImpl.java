a7942f5df62b3517e512d6cc5b6c4858636d7196
cutehanchankyo <jeongchangyo71@gmail.com>
2023-01-11 01:01:55 +0900

    add logout Service

--- src/main/java/com/example/demo/domain/service/Impl/MemberServiceImpl.java
+++ src/main/java/com/example/demo/domain/service/Impl/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.domain.service.Impl;
 
 import com.example.demo.domain.Member;
 import com.example.demo.domain.dto.request.MemberReqDto;
+import com.example.demo.domain.dto.request.SignInDto;
 import com.example.demo.domain.repository.MemberRepository;
 import com.example.demo.domain.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.DuplicateFormatFlagsException;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,13 +28,36 @@ public class MemberServiceImpl implements MemberService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public Long join(MemberReqDto memberReqDto){
-        if(!memberRepository.findOneByEmail(memberReqDto.getEmail()).isEmpty()){
+        if(!memberRepository.findByEmail(memberReqDto.getEmail()).isEmpty()){
             throw new DuplicateFormatFlagsException("Memeber already exists", ErrorCode.DUPLICATE_MEMBER);
         }
         Member member = memberReqDto.toEntity(passwordEncoder.encode(memberReqDto.getPassword()));
         return memberRepository.save(member).getId();
     }
 
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public Map<String, Objects> login(SignInDto signInDto){
+        Optional<Member> byEmail  = memberRepository.findByEmail(signInDto.getEmail());
+        if(byEmail.isEmpty()){
+            throw new MemberNotFindException("Can't find member by email",ErrorCode.MEMBER_NOT_FIND);
+        }
+        Member member = byEmail.get();
+        if(!passwordEncoder.matches(signInDto.getPassword(), member.getPassword())){
+            throw new PasswordNotFindException("Password Not Matches", ErrorCode.PASSWORD_NOT_CORRECT);
+        }
+        String accessToken = tokenProvider.generateAccessToken(member.getEmail());
+        String refreshToken = tokenProvider.generateRefreshToken(member.getEmail());
+        member.updateRefrachToken(refreshToken);
+        Map<String, Objects> loginResponse = getLoginResponse(member, accessToken, refreshToken);
+        return loginResponse;
+    }
 
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void logout(){
+        Member member = currentMemberUtil.getCurrentMember();
+        member.updateRefrachToken(null);
+    }
 
 }
