e5db77be85fd92ab57ae5a55375807daa7a77040
cutehanchankyo <jeongchangyo71@gmail.com>
2023-01-11 20:30:12 +0900

    add Contoller

--- src/main/java/com/example/demo/domain/contoller/AdminController.java
+++ src/main/java/com/example/demo/domain/contoller/AdminController.java
@@ -1,4 +1,34 @@
 package com.example.demo.domain.contoller;
 
+import com.example.demo.domain.dto.response.MemberResDto;
+import com.example.demo.domain.service.AdminService;
+import com.example.demo.domain.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/admin")
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final MemberService memberService;
+    private final AdminService adminService;
+    private final ResponseService responseService;
+
+    @GetMapping("/member/{memberIdx}")
+    public SingleResultResponse<MemberResDto> getOneMember(@PathVariable Long memberIdx){
+        return responseService.getSingleResult(memberService.getMemberByIdx(memberIdx));
+    }
+
+    @GetMapping("/member")
+    public ListResultResponse<MemberResDto> getAllmember(){
+        return responseService.getListResult(memberService.getAllMember());
+    }
+
+    @PatchMapping("/grant/member/{memberIdx}")
+    public CommonResultResponse grantRole(@PathVariable Long memberIdx){
+        adminService.grantRole(memberIdx);
+        return responseService.getSuccessResult();
+    }
+
 }
--- src/main/java/com/example/demo/domain/contoller/RefrashTokenController.java
+++ src/main/java/com/example/demo/domain/contoller/RefrashTokenController.java
@@ -1,32 +1,12 @@
 package com.example.demo.domain.contoller;
 
-import com.example.demo.domain.dto.response.MemberResDto;
-import com.example.demo.domain.service.AdminService;
-import com.example.demo.domain.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/admin")
+@RequestMapping
 @RequiredArgsConstructor
 public class RefrashTokenController {
-    private final MemberService memberService;
-    private final AdminService adminService;
-    private final ResponseService responseService;
 
-    @GetMapping("/member/{memberIdx}")
-    public SingleResultResponse<MemberResDto> getOneMember(@PathVariable Long memberIdx){
-        return responseService.getSingleResult(memberService.getMemberByIdx(memberIdx));
-    }
-
-    @GetMapping("/member")
-    public ListResultResponse<MemberResDto> getAllmember(){
-        return responseService.getListResult(memberService.getAllMember());
-    }
-
-    @PatchMapping ("/grant/member/{memberIdx}")
-    public CommonResultResponse grantRole(@PathVariable Long memberIdx){
-        adminService.grantRole(memberIdx);
-        return responseService.getSuccessResult();
-    }
 }
