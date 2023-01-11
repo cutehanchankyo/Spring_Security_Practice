6bbab6b4be3598509b9d32eb9734bc78dcbd42dd
cutehanchankyo <jeongchangyo71@gmail.com>
2023-01-11 20:35:40 +0900

    add ResfrashToken Controller

--- src/main/java/com/example/demo/domain/contoller/RefrashTokenController.java
+++ src/main/java/com/example/demo/domain/contoller/RefrashTokenController.java
@@ -1,12 +1,18 @@
 package com.example.demo.domain.contoller;
 
+import com.example.demo.domain.service.TokenRefrachService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/v1")
 @RequiredArgsConstructor
 public class RefrashTokenController {
+    private final ResponseSeruvice responseSeruvice;
+    private final TokenRefrachService tokenRefrachService;
 
+    @PostMapping("/refrash")
+    public SingleResultResponse refreshAccessToken(@RequestHeader String refreshToken){
+        return responseSeruvice.getSingleResult(tokenRefrachService.refrash(refreshToken));
+    }
 }
