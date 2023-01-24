a62199af0998d55fabd04bb23699f2fe154e5aad
cutehanchankyo <jeongchangyo71@gmail.com>
2023-01-24 23:22:27 +0900

    add

--- src/main/java/com/example/demo/domain/Member.java
+++ src/main/java/com/example/demo/domain/Member.java
@@ -26,6 +26,7 @@ public class Member {
 
     private String refrashToken;
 
+
     @Enumerated(EnumType.STRING) @Column(name = "Role")
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "Role", joinColumns = @JoinColumn(name = "id"))
