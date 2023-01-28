c855150fb68f26aa4f4015ccc17b52641c3cbae9
cutehanchankyo <jeongchangyo71@gmail.com>
2023-01-28 14:13:51 +0900

    add GlobalExceptionHandler

--- src/main/java/com/example/demo/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/global/exception/handler/GlobalExceptionHandler.java
@@ -1,16 +1,14 @@
 package com.example.demo.global.exception.handler;
 
 import com.example.demo.global.exception.ErrorResponse;
-import com.example.demo.global.exception.error.DuplicateMemberException;
-import com.example.demo.global.exception.error.TokenExpiredException;
+import com.example.demo.global.exception.error.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 @Slf4j
 @RestControllerAdvice
@@ -30,6 +28,31 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
     }
 
+    @ExceptionHandler(MemberNotFindException.class)
+    public ResponseEntity<ErrorResponse> MemberNotFindExceprionHandler(HttpServletRequest request, TokenExpiredException ex){
+        printError(request, ex, ex.getErrorCode().getMsg());
+        ErrorResponse errorResponse = new ErrorResponse(ex.getErrorCode().isSuccess(), ex.getErrorCode().getMsg(),ex.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
+    }
 
+    @ExceptionHandler(PasswordNotCorrectException.class)
+    public ResponseEntity<ErrorResponse> PasswordNotCorrectExceptionHandler(HttpServletRequest request, PasswordNotCorrectException ex){
+        printError(request, ex, ex.getErrorCode().getMsg());
+        ErrorResponse errorResponse = new ErrorResponse(ex.getErrorCode().isSuccess(), ex.getErrorCode().getMsg(), ex.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
+    }
+
+    @ExceptionHandler(TokenNotValidException.class)
+    public ResponseEntity<ErrorResponse> TokenNotValidExceptionHandler(HttpServletRequest request, TokenNotValidException ex){
+        printError(request, ex, ex.getErrorCode().getMsg());
+        ErrorResponse errorResponse = new ErrorResponse(ex.getErrorCode().isSuccess(), ex.getErrorCode().getMsg(), ex.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
+    }
+
+    private void printError(HttpServletRequest request,RuntimeException ex, String message){
+        log.error(request.getRequestURI());
+        log.error(message);
+        ex.printStackTrace();
+    }
 }
 
